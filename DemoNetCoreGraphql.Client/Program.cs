7bc1daa16c93170e5601d86df45877cd9626f8b9
Pete Chen <pisces72297@gmail.com>
2022-06-12 22:26:55 +0800

    add client

---
+++ DemoNetCoreGraphql.Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using DemoNetCoreGraphql.Client.Responses.FirstResponse;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using System.Text.Json;
+
+using var graphQLClient = new GraphQLHttpClient("https://localhost:9120/api/default", new SystemTextJsonSerializer());
+
+var personAndFilmsRequest = new GraphQLRequest
+{
+    Query = @"
+        query (
+          $id: Int!
+          $name: String!
+          $display: Boolean!
+        ) {
+          first (
+            input: { 
+              id: $id
+              name: $name
+            }
+          ) {
+            id
+            name @include(if: $display)
+          }
+        }
+	",
+    //   OperationName = "OperationName",
+    Variables = new
+    {
+		id = 9527,
+		name = "asdfg",
+		display = true,
+    },
+};
+
+var graphQLResponse = await graphQLClient.SendQueryAsync<FirstResponse>(personAndFilmsRequest);
+Console.WriteLine("raw response:");
+Console.WriteLine(JsonSerializer.Serialize(graphQLResponse, 
+    new JsonSerializerOptions 
+    { 
+        WriteIndented = false 
+    }));
+
+Console.WriteLine($"Id: {graphQLResponse.Data.First!.Id}");
+Console.WriteLine($"Name: {graphQLResponse.Data.First!.Name}");
+
+Console.WriteLine();
+Console.WriteLine("Press any key to quit...");
+Console.ReadKey();
+
---
+++ DemoNetCoreGraphql.Client/Responses/FirstResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNetCoreGraphql.Client.Responses.FirstResponse
+{
+    public class FirstResponse
+    {
+        public FirstDataResponse? First { get; set; }
+    }
+    public class FirstDataResponse
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
